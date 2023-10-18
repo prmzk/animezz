532281de8f36bf96b26b9ce7e89ece60b609a69d
Adam Primarizki <adam.primarizki@gmail.com>
2023-10-18 18:55:51 +0700

    add homepage and banner

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -7,21 +7,24 @@ import Tooltip from "./ui/tooltip";
 export default function Navigation() {
   return (
     <nav>
-      <div className="max-w-6xl mx-auto p-6 w-full flex justify-between">
-        <Link href="/">
-          <Image
-            src="/animezz.webp"
-            height={120 / 3}
-            width={593 / 3}
-            alt="animezz"
-          />
-        </Link>
-        <div>
-          <ul className="flex gap-2">
+      <div className="max-w-6xl mx-auto p-6 w-full flex justify-between items-center flex-wrap gap-2">
+        <div className="flex-shrink-0 w-32 md:w-auto">
+          <Link href="/">
+            <Image
+              src="/animezz.webp"
+              height={120 / 3}
+              width={593 / 3}
+              alt="animezz"
+            />
+          </Link>
+        </div>
+        <div className="ml-auto">
+          <ul className="flex">
             <li>
               <Button variant="link" asChild>
-                <Link href="/bookmarks">
-                  Your Bookmarks <Bookmark className="ml-2 h-4 w-4" />
+                <Link href="/bookmarks" className="text-right">
+                  Your Bookmarks{" "}
+                  <Bookmark className="ml-2 h-4 w-4 flex-shrink-0" />
                 </Link>
               </Button>
             </li>
